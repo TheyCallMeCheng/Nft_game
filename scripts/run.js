36cbd16d4b2dc9817ef8f9daf7fc6729195492b5
TheyCallMeCheng <cheng-corp@protonmail.com>
2022-06-19 14:50:57 +0200

    skeleton of the contract + updated deploy script

--- scripts/run.js
+++ scripts/run.js
@@ -1,6 +1,17 @@
 const main = async () => {
     const gameContractFactory = await hre.ethers.getContractFactory("MyEpicGame");
-    const gameContract = await gameContractFactory.deploy();
+    // Initialize the contract with the basic characters 
+    const gameContract = await gameContractFactory.deploy(
+        ["Anita", "arthur", "Jami"], //names
+        [
+            "https://i.imgur.com/pKd5Sdk.png", // Images
+            "https://i.imgur.com/xVu4vFL.png", 
+            "https://i.imgur.com/WMB6g9u.png"
+        ],
+        [1000,5000,2500], //HP and MaxHp values
+        [100, 25, 70] // Attack damage values 
+    );
+
     await gameContract.deployed();
     console.log("Contract deployed to: ", gameContract.address);
 }
